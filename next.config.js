900e54a2598b81290b271ad03b2e1afba0a1151c
pistachi73 <oscarpulido98@gmail.com>
2024-10-26 21:37:58 +0200

    Add trailinsh slash

--- next.config.js
+++ next.config.js
@@ -6,6 +6,8 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
+  trailingSlash: true,
+  reactStrictMode: false,
   images: {
     remotePatterns: [
       {
